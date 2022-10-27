ada34f1013b9b09de761811f5505a76df5d43e8d
brightchi <chinedu.ezeigbo@mtn.com>
2022-10-27 19:41:55 +0100

    Initial commit

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -3,9 +3,17 @@ $(document).contextmenu(function(){
 });
 
 $(document).ready(function() {
+	let resetKey = false;
+
 	$(window).click(function(event) {
-		if ($(event.target).is($("#myModal")) && $("#myModal").is(":visible")) {
-	    	$("#myModal").removeClass("show").addClass("hide");
+		if ($(event.target).is($("#myModal_")) && $("#myModal_").is(":visible")) {
+			if(resetKey) {
+				reset();
+			}
+
+			$("#info").html("");
+			$("#info").removeClass("show_").addClass("hide_");
+	    	$("#myModal_").removeClass("show_").addClass("hide_");
 	  	}
 	});
 
@@ -19,6 +27,13 @@ $(document).ready(function() {
 		e.preventDefault();
 		processImpl();
 	});
+
+	function reset() {
+		$("#info").html("");
+		$('#message').val("");
+		$('#fullname').val("");
+		$($('#image')[0]).val("");
+	}
 
 	function uploadToCloud(file) {
 		let imageUrl = null;
@@ -39,47 +54,98 @@ $(document).ready(function() {
 
 		return imageUrl;
 	}
+
+	function validateImageFormat(fileName) {
+		return fileName.endsWith(".jpg") || fileName.endsWith(".png") || fileName.endsWith(".jpeg")
+			|| fileName.endsWith(".JPG") || fileName.endsWith(".PNG") || fileName.endsWith(".JPEG")
+	}
 	
 	function processImpl() {
-		const MAX_FILE_SIZE = 4000000;
+		$("#myModal_").removeClass("hide_").addClass("show_");
+		$("#loader_div").removeClass("hide_").addClass("show_");
+
+		const MAX_FILE_SIZE = 25000000;
+		const MAX_MESSAGE_LENGTH = 450;
 		let fullname = $('#fullname').val();
 		let message = $('#message').val();
 		let image = $('#image')[0].files[0];
 		const SEND_MESSAGE_ENDPOINT = "/api/v1/send";
 
-		if(image.size <= MAX_FILE_SIZE) {
-			let imageURL = uploadToCloud(image);
+		if(image) {
+			if(image.size <= MAX_FILE_SIZE) {
+				if(validateImageFormat(image.name)) {
+					if(message.length <= MAX_MESSAGE_LENGTH) {
+						if(fullname.match(/^([A-Za-z -']+)$/)) {
+							resetKey = true;
+							let imageURL = uploadToCloud(image);
+							// let imageURL = "uploadToCloud(image);" //dummy
 
-			let userDTO = {
-				'firstname': fullname,
-				'lastname': fullname,
-				'imageLink': imageURL
-			};
+							let userDTO = {
+								'firstname': fullname,
+								'lastname': fullname,
+								'imageLink': imageURL
+							};
 
-			let messageDTO = {
-				'message': message,
-				'userDTO': userDTO
-			};
+							let messageDTO = {
+								'message': message,
+								'userDTO': userDTO
+							};
 
-			$.ajax({
-				type: "POST",
-				url: SEND_MESSAGE_ENDPOINT,
-				contentType: "application/json; charset=utf-8",
-				// dataType: 'json',
-				processData: false,
-				data: JSON.stringify(messageDTO),
-				cache: false,
-				timeout: 600000,
-				success: function (data) {
-					console.log(data);
-				},
-				error: function(data) {
-					console.log("--->> Payload", messageDTO);
-					console.log("--->> Error", data);
+							$.ajax({
+								type: "POST",
+								url: SEND_MESSAGE_ENDPOINT,
+								contentType: "application/json; charset=utf-8",
+								processData: false,
+								data: JSON.stringify(messageDTO),
+								cache: false,
+								timeout: 600000,
+								success: function (data) {
+									let msg = "<h4>" + data + "</h4><p>Thank you!</p>"
+									$("#loader_div").removeClass("show_").addClass("hide_");
+									$("#info").removeClass("hide_").addClass("show_");
+									$("#info").html(msg);
+								},
+								error: function(data) {
+									let msg = "<h4>Message Delivery Failed</h4><p>Kindly try again later</p>"
+									$("#loader_div").removeClass("show_").addClass("hide_");
+									$("#info").removeClass("hide_").addClass("show_");
+									$("#info").html(msg);
+								}
+							});
+						} else {
+							resetKey = false;
+							let msg = "<h4>Invalid Fullname</h4><p>Kindly use valid characters in you name(s).</p>"
+							$("#loader_div").removeClass("show_").addClass("hide_");
+							$("#info").removeClass("hide_").addClass("show_");
+							$("#info").html(msg);
+						}
+					} else{
+						resetKey = false;
+						let msg = "<h4>Message Too Long</h4><p>Kindly make your message concise and resend.</p>"
+						$("#loader_div").removeClass("show_").addClass("hide_");
+						$("#info").removeClass("hide_").addClass("show_");
+						$("#info").html(msg);
+					}
+				} else {
+					resetKey = false;
+					let msg = "<h4>Invalid Image</h4><p>Image format not allowed. Kindly upload jpg or png format</p>"
+					$("#loader_div").removeClass("show_").addClass("hide_");
+					$("#info").removeClass("hide_").addClass("show_");
+					$("#info").html(msg);
 				}
-			});
+			} else {
+				resetKey = false;
+				let msg = "<h4>Image Quite Large</h4><p>Image size is too large, required size should be less than 25MB</p>"
+				$("#loader_div").removeClass("show_").addClass("hide_");
+				$("#info").removeClass("hide_").addClass("show_");
+				$("#info").html(msg);
+			}
 		} else {
-
+			resetKey = false;
+			let msg = "<h4>Image Required</h4><p>Kindly upload a picture of you</p>"
+			$("#loader_div").removeClass("show_").addClass("hide_");
+			$("#info").removeClass("hide_").addClass("show_");
+			$("#info").html(msg);
 		}
 	}
 });
